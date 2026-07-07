20087b355bae4e9139dcd12a6ef309c447c8453c
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-07-07 12:07:38 +0200

    add sidebar section in app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-100/30 text-slate-950">
       <Header />
+
+      <main className="mx-auto max-w-7xl px-4 py-6">
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
+          <Sidebar />
+        </div>
+        </main>
+
     </div>
   );
 }
